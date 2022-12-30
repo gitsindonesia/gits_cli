55ac49a64733698668fe3122a1c41ac6e3f8a1d6
Dena Meidina <dena@gits.id>
2022-12-30 08:19:43 +0700

    docs: update changelog to 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- docs: add documentation in readme
+
 ## 1.0.0
 
 - Initial version.
--- bin/gits_cli.dart
+++ bin/gits_cli.dart
@@ -49,7 +49,7 @@ void main(List<String> arguments) {
   try {
     final results = runner.argParser.parse(arguments);
     if (results.wasParsed('version')) {
-      print('Gits CLI 1.0.0');
+      print('Gits CLI 1.0.1');
       exit(0);
     }
   } catch (e) {
