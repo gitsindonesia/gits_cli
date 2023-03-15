19129c56286451185f42f73979c5dcc9aa831e60
Dena Meidina <dena@gits.id>
2023-03-15 12:04:18 +0700

    docs: update documentation

--- bin/gits_cli.dart
+++ bin/gits_cli.dart
@@ -51,7 +51,7 @@ void main(List<String> arguments) {
   try {
     final results = runner.argParser.parse(arguments);
     if (results.wasParsed('version')) {
-      print('Gits CLI 1.3.0+1');
+      print('Gits CLI 1.3.0+2');
       exit(0);
     }
   } catch (e) {
